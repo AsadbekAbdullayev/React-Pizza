c514bbb4d8a42e6d27c38a802e61cc1e442eca67
AsadbekAbdullayev <asadbekmmm127@gmail.com>
2023-11-05 11:45:39 +0500

    test1

--- src/root/index.js
+++ src/root/index.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import Korzinka from '../page/korzinka';
-import Home from '../page/home';
-import Admin from '../page/admin';
-import { Routes,Route,Navigate } from 'react-router';
+import React from "react";
+import Korzinka from "../page/korzinka";
+import Home from "../page/home";
+import Admin from "../page/admin";
+import { Routes, Route, Navigate } from "react-router";
 
 const Root = () => {
-  return <>
-   <Routes>
-  
-    <Route  path='korzinka' element={<Korzinka/>}/>
-    <Route  path='admin' element={<Admin/>}/>
-    <Route  path='home' element={<Home/>}/>
-    <Route path='/' element={<Navigate to={'/home'}/>} />
-    <Route path='*' element={<h1>NOT FOUND</h1>} />
-
-  </Routes>
-  
-  </>
+  return (
+    <>
+      <Routes>
+        {/* this is from test1 branch */}
+        <Route path="korzinka" element={<Korzinka />} />
+        <Route path="admin" element={<Admin />} />
+        <Route path="home" element={<Home />} />
+        <Route path="/" element={<Navigate to={"/home"} />} />
+        <Route path="*" element={<h1>NOT FOUND</h1>} />
+      </Routes>
+    </>
+  );
 };
 
 export default Root;
